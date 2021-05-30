875590859d8111739ef12a1a496f61320ff3a0b9
Aleks <nikolic.aleks90@gmail.com>
2021-05-30 14:17:11 -0400

    new script for toggling email icon pneat but still needs to toggle off properly

--- scripts/emailHover.js
+++ scripts/emailHover.js
@@ -3,6 +3,16 @@ const emailAnchor = document.querySelector('.footerEmail');
 
 emailAnchor.addEventListener('mouseover', () => {
 
-  emailLogo.classList.toggle('fa-envelope');
-  emailLogo.classList.add('fa-envelope-open')
+
+  if (emailLogo.classList.contains('fa-envelope')) {
+
+    emailLogo.classList.remove('fa-envelope');
+    emailLogo.classList.add('fa-envelope-open');
+
+  } else if (emailLogo.classList.contains('fa-envelope-open')) {
+
+    emailLogo.classList.remove('fa-envelope-open');
+    emailLogo.classList.add('fa-envelope');
+  }
+
 })
