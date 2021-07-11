ec62c510618814a8b5d9ecdcb862faf44b9d3543
Aleks <nikolic.aleks90@gmail.com>
2021-07-11 11:52:38 -0400

    fixed a small mouseover design issue

--- scripts/emailHover.js
+++ scripts/emailHover.js
@@ -3,16 +3,14 @@ const emailAnchor = document.querySelector('.footerEmail');
 
 emailAnchor.addEventListener('mouseover', () => {
 
+  emailLogo.classList.remove('fa-envelope');
+  emailLogo.classList.add('fa-envelope-open');
 
-  if (emailLogo.classList.contains('fa-envelope')) {
+})
 
-    emailLogo.classList.remove('fa-envelope');
-    emailLogo.classList.add('fa-envelope-open');
+emailAnchor.addEventListener('mouseout', () => {
 
-  } else if (emailLogo.classList.contains('fa-envelope-open')) {
-
-    emailLogo.classList.remove('fa-envelope-open');
-    emailLogo.classList.add('fa-envelope');
-  }
+  emailLogo.classList.remove('fa-envelope-open');
+  emailLogo.classList.add('fa-envelope');
 
 })
